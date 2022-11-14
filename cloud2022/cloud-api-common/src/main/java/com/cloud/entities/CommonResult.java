7165ffa91963edff5f89e34edd78092c0ff8e7b2
theodore <616364596@qq.com>
2022-11-14 20:44:45 +0800

    common

--- cloud2022/cloud-api-common/src/main/java/com/cloud/entities/CommonResult.java
+++ cloud2022/cloud-api-common/src/main/java/com/cloud/entities/CommonResult.java
@@ -1,4 +1,4 @@
-package com.xiaotu.cloud.entities;
+package com.cloud.entities;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
---
+++ cloud2022/cloud-consumer-order80/src/main/java/com/xiaotu/cloud/controller/OrderController.java
@@ -0,0 +1,36 @@
+package com.xiaotu.cloud.controller;
+
+
+import com.xiaotu.cloud.entities.CommonResult;
+import com.xiaotu.cloud.entities.Payment;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+@RestController
+@Slf4j
+public class OrderController {
+
+    private static final String PAYMENT_URL= "http://localhost:8001";
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping("/consumer/payment/create")
+    public CommonResult<Payment> create(Payment payment){
+        log.info("插入的数据="+payment);
+        return restTemplate.postForObject(PAYMENT_URL+"/payment/create",payment,CommonResult.class);
+    }
+
+    @GetMapping("/consumer/payment/get/{id}")
+    public CommonResult<Payment> getPayment(@PathVariable("id") Long id){
+        log.info("查询id="+id);
+        return restTemplate.getForObject(PAYMENT_URL+"/payment/id/" +id,CommonResult.class);
+    }
+
+}
